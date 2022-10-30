74765ea73830b8b8987bf53d6673c3838e9226b8
griinzy <kalin_stoqnov@abv.bg>
2022-10-31 00:41:27 +0200

    header without checked colors

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const { app, BrowserWindow } = require('electron')
 const createWindow = () => {
     const win = new BrowserWindow({
-      width: 800,
-      height: 600
+      //width: 800,
+      //height: 600
+      autoHideMenuBar: true
     })
+    //win.removeMenu() 
   
     win.loadFile('index.html')
   }
