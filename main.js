fcec556cf047c8e14884bb55be4fde7927728e1a
griinzy <kalin_stoqnov@abv.bg>
2022-10-31 01:27:41 +0200

    woo transitions

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const { app, BrowserWindow } = require('electron')
 const createWindow = () => {
     const win = new BrowserWindow({
-      //width: 800,
-      //height: 600
+      width: 800,
+      height: 600,
       autoHideMenuBar: true
     })
     //win.removeMenu() 
